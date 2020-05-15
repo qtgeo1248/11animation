19790ae6d533f111950e57c164b186608095954c
qtgeo1248 <gzhou7819@gmail.com>
2020-05-14 20:36:18 -0400

    setup2

--- script.py
+++ script.py
@@ -18,11 +18,9 @@ from draw import *
   to some default value, and print out a message
   with the name being used.
   ==================== """
-def first_pass( commands ):
-
+def first_pass(commands):
     name = ''
     num_frames = 1
-
     return (name, num_frames)
 
 """======== second_pass( commands ) ==========
@@ -42,9 +40,8 @@ def first_pass( commands ):
   dictionary corresponding to the given knob with the
   appropirate value.
   ===================="""
-def second_pass( commands, num_frames ):
-    frames = [ {} for i in range(num_frames) ]
-
+def second_pass(commands, num_frames):
+    frames = [{} for i in range(num_frames)]
     return frames
 
 
@@ -60,34 +57,23 @@ def run(filename):
         print("Parsing failed.")
         return
 
-    view = [0,
-            0,
-            1];
-    ambient = [50,
-               50,
-               50]
-    light = [[0.5,
-              0.75,
-              1],
-             [255,
-              255,
-              255]]
-
+    view = [0, 0, 1];
+    ambient = [50, 50, 50]
+    light = [[0.5, 0.75, 1],
+             [255, 255, 255]]
     color = [0, 0, 0]
-    symbols['.white'] = ['constants',
-                         {'red': [0.2, 0.5, 0.5],
-                          'green': [0.2, 0.5, 0.5],
-                          'blue': [0.2, 0.5, 0.5]}]
+    symbols['.white'] = ['constants', {'red': [0.2, 0.5, 0.5],
+                                       'green': [0.2, 0.5, 0.5],
+                                       'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
     (name, num_frames) = first_pass(commands)
     frames = second_pass(commands, num_frames)
 
-
     tmp = new_matrix()
-    ident( tmp )
+    ident(tmp)
 
-    stack = [ [x[:] for x in tmp] ]
+    stack = [[x[:] for x in tmp]]
     screen = new_screen()
     zbuffer = new_zbuffer()
     tmp = []
@@ -101,39 +87,33 @@ def run(filename):
         c = command['op']
         args = command['args']
         knob_value = 1
-
         if c == 'box':
             if command['constants']:
                 reflect = command['constants']
-            add_box(tmp,
-                    args[0], args[1], args[2],
-                    args[3], args[4], args[5])
-            matrix_mult( stack[-1], tmp )
+            add_box(tmp, args[0], args[1], args[2], args[3], args[4], args[5])
+            matrix_mult(stack[-1], tmp)
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
             reflect = '.white'
         elif c == 'sphere':
             if command['constants']:
                 reflect = command['constants']
-            add_sphere(tmp,
-                       args[0], args[1], args[2], args[3], step_3d)
-            matrix_mult( stack[-1], tmp )
+            add_sphere(tmp, args[0], args[1], args[2], args[3], step_3d)
+            matrix_mult(stack[-1], tmp)
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
             reflect = '.white'
         elif c == 'torus':
             if command['constants']:
                 reflect = command['constants']
-            add_torus(tmp,
-                      args[0], args[1], args[2], args[3], args[4], step_3d)
-            matrix_mult( stack[-1], tmp )
+            add_torus(tmp, args[0], args[1], args[2], args[3], args[4], step_3d)
+            matrix_mult(stack[-1], tmp)
             draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
             tmp = []
             reflect = '.white'
         elif c == 'line':
-            add_edge(tmp,
-                     args[0], args[1], args[2], args[3], args[4], args[5])
-            matrix_mult( stack[-1], tmp )
+            add_edge(tmp, args[0], args[1], args[2], args[3], args[4], args[5])
+            matrix_mult(stack[-1], tmp)
             draw_lines(tmp, screen, zbuffer, color)
             tmp = []
         elif c == 'move':
@@ -165,4 +145,3 @@ def run(filename):
             display(screen)
         elif c == 'save':
             save_extension(screen, args[0])
-        # end operation loop
